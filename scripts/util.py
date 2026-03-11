f22e894ec24084dcada99fa598b2ea3f6e2caaa4
Jefferson Deyvis <67497412+JeffersonDeyvis@users.noreply.github.com>
2026-03-11 11:09:14 -0300

    add view of elapsed time

---
+++ scripts/util.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+
+
+def get_time_elapsed(ansatz='qcnn'):
+    ansatz = ansatz.upper()
+    mean_time = []
+    std_time = []
+
+    # Range 100 a 1000 (inclusive) de 50 em 50
+    for step in range(100, 1050, 50):
+
+        f_name = f'../results/training/{ansatz}/{ansatz}_B04S{step}_TIME_MEAN.npy'
+
+        if os.path.exists(f_name):
+            data = np.loadtxt(f_name)
+            mean_time.append(np.mean(data))
+            std_time.append(np.std(data))
+        else:
+            print(f"Aviso: Arquivo {f_name} não encontrado.")
+
+    return np.array(mean_time), np.array(std_time)
