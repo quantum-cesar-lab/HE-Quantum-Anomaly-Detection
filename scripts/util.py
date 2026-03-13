864a0e11ebcf84c300d14b9cbd49edf7a8279e83
Jefferson Deyvis <67497412+JeffersonDeyvis@users.noreply.github.com>
2026-03-13 13:42:53 -0300

    experiment
    experimento par calcular o tempo gasto diminuindo a taxa de aprendizado para 10x menor

--- scripts/util.py
+++ scripts/util.py
@@ -20,3 +20,22 @@ def get_time_elapsed(ansatz='qcnn'):
             print(f"Aviso: Arquivo {f_name} não encontrado.")
 
     return np.array(mean_time), np.array(std_time)
+
+
+def get_time_elapsed2(ansatz='qcnn'):
+    ansatz = ansatz.upper()
+    mean_time = []
+
+    # Range 100 a 1000 (inclusive) de 50 em 50
+    for step in range(50, 500, 50):
+        print(step)
+
+        f_name = f'../results/training/{ansatz}/{ansatz}_B04S{step}_TIME_SINGLE_EXEC.npy'
+
+        if os.path.exists(f_name):
+            data = np.loadtxt(f_name)
+            mean_time.append(data)
+        else:
+            print(f"Aviso: Arquivo {f_name} não encontrado.")
+
+    return np.array(mean_time)
