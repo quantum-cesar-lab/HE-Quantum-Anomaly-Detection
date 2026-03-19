2b7f33a389496ee832daa8bd1205d5fbbd8b71a3
jvfg-cesar <jvfg@cesar.org.br>
2026-03-19 16:21:22 -0300

    fix: minor changes to notebook cells

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,7 +7,6 @@ def get_time_elapsed(ansatz='qcnn'):
     mean_time = []
     std_time = []
 
-    # Range 100 a 1000 (inclusive) de 50 em 50
     for step in range(100, 1050, 50):
 
         f_name = f'../results/training/{ansatz}/{ansatz}_B04S{step}_TIME_MEAN.npy'
@@ -17,7 +16,7 @@ def get_time_elapsed(ansatz='qcnn'):
             mean_time.append(np.mean(data))
             std_time.append(np.std(data))
         else:
-            print(f"Aviso: Arquivo {f_name} não encontrado.")
+            print(f"Warning: File {f_name} not found.")
 
     return np.array(mean_time), np.array(std_time)
 
@@ -26,7 +25,6 @@ def get_time_elapsed2(ansatz='qcnn'):
     ansatz = ansatz.upper()
     mean_time = []
 
-    # Range 100 a 1000 (inclusive) de 50 em 50
     for step in range(50, 500, 50):
         print(step)
 
@@ -36,6 +34,6 @@ def get_time_elapsed2(ansatz='qcnn'):
             data = np.loadtxt(f_name)
             mean_time.append(data)
         else:
-            print(f"Aviso: Arquivo {f_name} não encontrado.")
+            print(f"Warning: File {f_name} not found.")
 
     return np.array(mean_time)
