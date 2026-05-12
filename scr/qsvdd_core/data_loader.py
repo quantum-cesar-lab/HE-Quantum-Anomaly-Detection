9dc4671a2e292e17ca1f68d649601e53b722161b
jvfg-cesar <jvfg@cesar.org.br>
2026-05-12 12:17:52 -0300

    feat: new notebook for qsvdd experiments using breast cancer dataset

--- scr/qsvdd_core/data_loader.py
+++ scr/qsvdd_core/data_loader.py
@@ -36,6 +36,23 @@ class QuantumDataLoader:
 
         return X_quantum, y
 
+    def prepare_bc_data(self, x_data):
+
+        X_scaled = self.scaler.fit_transform(x_data)
+
+        n_samples, n_features = X_scaled.shape
+        padding_size = self.target_dim - n_features
+        X_padded = np.pad(
+            X_scaled, ((0, 0), (0, padding_size)), mode="constant", constant_values=0
+        )
+
+        norms = np.linalg.norm(X_padded, axis=1, keepdims=True)
+        # Avoids division by zero
+        norms[norms == 0] = 1.0
+        X_quantum = X_padded / norms
+
+        return X_quantum
+
     def prepare_classic_data(self, df):
         # Flow for classic algorithms (IF, LOF, SVM, Deep-SVDD)
         X = df.drop("Class", axis=1).values
