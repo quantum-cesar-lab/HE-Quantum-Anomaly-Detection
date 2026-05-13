c6c5669bea9887e003f535fedb13e91bf9d2ccad
jvfg-cesar <jvfg@cesar.org.br>
2026-05-13 14:28:20 -0300

    test: more tests with different datasets

--- scr/qsvdd_core/data_loader.py
+++ scr/qsvdd_core/data_loader.py
@@ -52,6 +52,23 @@ class QuantumDataLoader:
         X_quantum = X_padded / norms
 
         return X_quantum
+    
+    def prepare_census_data(self, x_data):
+
+        X_scaled = self.scaler.fit_transform(x_data)
+
+        n_samples, n_features = X_scaled.shape
+        padding_size = self.target_dim - n_features
+        X_padded = np.pad(
+            X_scaled, ((0, 0), (0, padding_size)), mode="constant", constant_values=0
+        )
+
+        norms = np.linalg.norm(X_padded, axis=1, keepdims=True)
+        # Avoids division by zero
+        norms[norms == 0] = 1.0
+        X_quantum = X_padded / norms
+
+        return X_quantum
 
     def prepare_classic_data(self, df):
         # Flow for classic algorithms (IF, LOF, SVM, Deep-SVDD)
